14270d7675866ad56b96c38e2efaa5e221c5ff3c
leo-alexander <leo.mclay@gmail.com>
2017-02-12 17:20:48 +1300

    basic functionality

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+var problem = "";
+var solution = "";
+var key = "";
+
+//On any key pressed:
+$('.key').click(keyPressed);
+function keyPressed() {
+  if (this.classList.contains("num")) {//If key pressed is no., add to problem
+    problem += $(this).attr("value");
+    document.getElementById('problem').innerHTML = problem;
+  }
+  else if (this.classList.contains("arithmetic")) {//If key pressed is arithmetic, add to problem
+    problem += $(this).attr("value");
+    document.getElementById('problem').innerHTML = problem;
+  }
+  else if (this.classList.contains("allClear")) {//If key pressed is AC, clear display
+    problem="";
+    solution="";
+    document.getElementById('problem').innerHTML = problem;
+    document.getElementById('solution').innerHTML = solution;
+  }
+  else if (this.classList.contains("clearEntry")) {//If key pressed is DEL, remove last key
+    problem = problem.substring(0, problem.length-1);
+    document.getElementById('problem').innerHTML = problem;
+  }
+  else if (this.classList.contains("equals")) {//If key pressed is equals, caculate
+    calculate();
+  }
+}
+
+function calculate() {
+  solution = eval(problem);//Turn problem string into calculation
+  document.getElementById('solution').innerHTML = solution;//Display solution
+  document.getElementById('problem').innerHTML = "";//Reset problem
+  problem = "";//Reset problem display
+}
+});
