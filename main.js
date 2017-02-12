5177c5e75a9b2c610319895e654737808c051de1
leo-alexander <leo.mclay@gmail.com>
2017-02-12 18:23:24 +1300

    bug fix

--- main.js
+++ main.js
@@ -22,8 +22,14 @@ function keyPressed() {
     document.getElementById('solution').innerHTML = solution;
   }
   else if (this.classList.contains("clearEntry")) {//If key pressed is DEL, remove last key
+    if (problem == "") {
+      solution = ""
+      document.getElementById('solution').innerHTML = solution;
+    }
+    else {
     problem = problem.substring(0, problem.length-1);
     document.getElementById('problem').innerHTML = problem;
+    }
   }
   else if (this.classList.contains("equals")) {//If key pressed is equals, caculate
     calculate();
@@ -33,7 +39,7 @@ function keyPressed() {
 function calculate() {
   solution = eval(problem);//Turn problem string into calculation
   document.getElementById('solution').innerHTML = solution;//Display solution
-  document.getElementById('problem').innerHTML = "";//Reset problem
-  problem = "";//Reset problem display
+  document.getElementById('problem').innerHTML = "";//Reset problem display
+  problem = solution;
 }
 });
